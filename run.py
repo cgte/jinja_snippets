eda94c1f6079cd426660aeb2fdc6f97a6763af1e
Colin Goutte <cgte@bk.ru>
2020-11-04 15:54:31 +0100

    We now render the template

--- run.py
+++ run.py
@@ -3,3 +3,13 @@ from jinja2 import FileSystemLoader
 file_loader = FileSystemLoader("templates")
 
 print(f"templates: {file_loader.list_templates()}")
+
+
+from jinja2 import Environment  # noqa
+
+env = Environment(loader=file_loader)
+
+hello = env.get_template("montemplate.j2")
+name = "colin"
+
+print(hello.render(name=name))
