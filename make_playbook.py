5cd089bcee8a5c7f694a98596aa570fbc3258a13
Colin Goutte <cgte@bk.ru>
2020-11-06 12:15:53 +0100

    Check with multiple envs

--- make_playbook.py
+++ make_playbook.py
@@ -9,7 +9,45 @@ from jinja2 import Environment  # noqa
 
 env = Environment(loader=file_loader)
 
+
 hosts_template = env.get_template("tagged_hosts.j2")
 groups = {"group1": ["host1", "host2"], "group2": ["host2", "host3"]}
+
+
+print(hosts_template.render(groups=groups))
+
+import random  # noqa
+import string  # noqa
+
+tags = ["dev", "prepod", "integ", "valid", "prod"]
+
+
+class DummyNode:
+    def __init__(self):
+        self.tags = random.choices(tags, k=random.randint(1, len(tags)))
+        self.name = (
+            "".join(random.choices(string.ascii_lowercase, k=10))
+            + "_"
+            + "-".join(self.tags)
+        )
+
+    pass
+
+
+nodes = [DummyNode() for _ in range(10)]
+
+import operator  # noqa
+
+by_tags = operator.attrgetter("tags")
+
+from collections import defaultdict  # noqa
+
+groups = defaultdict(set)
+
+for node in nodes:
+    for tag in node.tags:
+        groups[tag].add(node.name)
+
+print("With many hosts")
 
 print(hosts_template.render(groups=groups))
